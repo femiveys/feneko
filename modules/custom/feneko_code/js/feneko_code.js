e71ce17dabae21847418252efac461235cab64ab
Femi Veys <femi@itsimplyworks.be>
2015-08-31 00:32:02 +0200

    Add filters to lopende orders

--- modules/custom/feneko_code/js/feneko_code.js
+++ modules/custom/feneko_code/js/feneko_code.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.fase_help = {
     attach: function (context, settings) {
-      $('.page-user-feneko-screens-orders .help').click(function() {
-        $('.page-user-feneko-screens-orders .help-text').slideToggle();
+      $('.page-user-feneko-orders .help').click(function() {
+        $('.page-user-feneko-orders .help-text').slideToggle();
 
         return false;
       });
